f3fd468697adfa2c5225f5842c21bd08894ac396
hussein <husseinhesham24@gmail.com>
2021-04-09 22:22:33 +0200

    Upload Project

---
+++ lib/ques.dart
@@ -0,0 +1,41 @@
+final QUES = [
+  {
+    "question": "What is the color of the sea?",
+    "answers": ["blue", "red", "green"],
+    "correctIndex": 0
+  },
+  {
+    "question": "what is the answer of 2+5?",
+    "answers": ["4", "3", "7"],
+    "correctIndex": 2
+  },
+  {
+    "question": "what is the most cuttness animal?",
+    "answers": [
+      "duck",
+      "dog",
+      "panda",
+    ],
+    "correctIndex": 1
+  },
+  {
+    "question": "What is the capital of Spain?",
+    "answers": ["Berlin", "alexandria", "madrid"],
+    "correctIndex": 2
+  },
+  {
+    "question":
+        "What are the school colors of the University of Texas at Austin?",
+    "answers": [
+      "black",
+      "orange",
+      "white",
+    ],
+    "correctIndex": 1
+  },
+  {
+    "question": "What is the hardest stone ?",
+    "answers": ["gold", "diamond", "iron"],
+    "correctIndex": 1
+  }
+];
